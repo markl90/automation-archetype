dd8a3bbfb9b306d5488ccba4a5f093034661821a
M Ledwold <you@example.com>
2019-09-16 22:22:56 +0100

    commit archetype project

---
+++ src/test/java/com/netbuilder/runners/TestRunner.java
@@ -0,0 +1,11 @@
+package com.netbuilder.runners;
+
+//@RunWith(Cucumber.class)
+//@CucumberOptions(
+//        features = "src/test/resources/featurefiles/",
+//        glue = { "com.netbuilder.steps" },
+//        tags = { "@test"},
+//        plugin = { "pretty", "html:target/test-reports", "json:target/cucumber-reports/Cucumber.json" }
+//)
+//public class TestRunner {
+//}
